84e3eb88b7fccfd8e83e541aa714872a632872f0
Benson Peng <penglingzhe@gmail.com>
2013-02-18 15:47:10 +0800

    update

--- test/com/orange/common/elasticsearch/test/QuickTest.java
+++ test/com/orange/common/elasticsearch/test/QuickTest.java
@@ -7,7 +7,10 @@ import java.util.Date;
 
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.deletebyquery.DeleteByQueryResponse;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
@@ -32,22 +35,31 @@ public class QuickTest {
 		try {
 			doc = jsonBuilder()  
 				      .startObject()       
-				          .field("title", "this is a title!")  
-				          .field("description", "descript what?")   
-				          .field("price", 100)  
-				          .field("onSale", true)  
-				          .field("type", 1)  
+				          .field("title", "this is a title 2!")  
+				          .field("description", "descript what 2?")   
+				          .field("price", 200)  
+				          .field("onSale", false)  
+				          .field("type", 2)  
 				          .field("createDate", new Date())                            
 				     .endObject();
-			IndexResponse indexResponse =  client.prepareIndex("productIndex","productType").setSource(doc).execute().actionGet();
-			System.out.println("return index="+indexResponse.toString());
+			IndexResponse indexResponse =  client.prepareIndex("product_index","product_type").setSource(doc).execute().actionGet();
+			System.out.println("return index="+indexResponse.getId());
 			
+			GetResponse response = client.prepareGet("product_index", "product_type", "1")
+					.setOperationThreaded(false)
+			        .execute()
+			        .actionGet();
+			System.out.println("return get="+response.getFields().toString());
+			
+			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}  
 		
-			
+		
+		/*
 		// delete by id
 		DeleteResponse response = client.prepareDelete("productIndex", "productType", "1")   
 		        .execute()   
@@ -66,15 +78,15 @@ public class QuickTest {
     	        .execute()   
     	        .actionGet(); 	      
 		System.out.println("return syncDeleteResponse="+syncDeleteResponse.toString());
-	      
+	    */
+	     
       // query
       QueryBuilder qb1 = termQuery("name", "kimchy");   
       
       QueryBuilder qb2 = boolQuery()   
-                          .must(termQuery("content", "test1"))   
-                          .must(termQuery("content", "test4"))   
-                          .mustNot(termQuery("content", "test2"))   
-                          .should(termQuery("content", "test3"));   
+                          .must(termQuery("price", 200));   
+//                          .mustNot(termQuery("content", "test2"))   
+//                          .should(termQuery("content", "test3"));   
          
       QueryBuilder qb3 = filteredQuery(   
                   termQuery("name.first", "shay"),    
@@ -88,4 +100,26 @@ public class QuickTest {
 		client.close();		
 	}
 
+//	@Test
+	public void testQuery1() {
+		Client client = new TransportClient().addTransportAddress(new InetSocketTransportAddress("localhost", 9300));   
+		
+		SearchResponse response1 = client.prepareSearch().execute().actionGet();
+		System.out.println("return response="+response1.getHits().getTotalHits());	      
+		
+	      QueryBuilder qb2 = boolQuery()   
+                  .must(termQuery("price", 200));   
+
+	      SearchResponse response = client.prepareSearch("product_index")   
+	    		  	.setTypes("product_type")
+	    		  	.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+	    		  	.setQuery(QueryBuilders.termQuery("title", "title"))
+	    	        .setFrom(0).setSize(60).setExplain(true)   
+	    	        .execute()   
+	    	        .actionGet();  	      
+	      
+			System.out.println("return response="+response.toString());	      
+	      
+	      client.close();
+	}
 }
