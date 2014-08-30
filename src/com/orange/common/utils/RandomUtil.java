8c98727444f30ce5710868ad32933413041147b0
Benson Peng <penglingzhe@gmail.com>
2014-08-30 12:07:29 +0800

    fix random number

--- src/com/orange/common/utils/RandomUtil.java
+++ src/com/orange/common/utils/RandomUtil.java
@@ -24,19 +24,38 @@ public class RandomUtil {
         return val;
     }
 
-    private static final String charset = //"!@#$%^&*()" +
+    private static final String NUMBER_LETTER = //"!@#$%^&*()" +
             "0123456789" +
             "abcdefghijklmnopqrstuvwxyz"; // +
 //            "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public static String randomString(int length) {
+        return randomString(NUMBER_LETTER, length);
+//        Random rand = new Random(System.currentTimeMillis());
+//        StringBuffer sb = new StringBuffer();
+//        int charSetLen = charset.length();
+//        for (int i = 0; i <= length; i++ ) {
+//            int pos = rand.nextInt(charSetLen);
+//            sb.append(charset.charAt(pos));
+//        }
+//        return sb.toString();
+    }
+
+    private static final String NUMBER = "0123456789";
+
+    public static String randomNumberString(int length){
+        return randomString(NUMBER, length);
+    }
+
+    public static String randomString(String charset, int length) {
         Random rand = new Random(System.currentTimeMillis());
         StringBuffer sb = new StringBuffer();
         int charSetLen = charset.length();
-        for (int i = 0; i <= length; i++ ) {
+        for (int i = 0; i < length; i++ ) {
             int pos = rand.nextInt(charSetLen);
             sb.append(charset.charAt(pos));
         }
         return sb.toString();
     }
+
 }
